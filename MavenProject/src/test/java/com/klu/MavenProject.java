1f770f2eb02c80ad8362a35850ee367f74e06fec
2400031510- vijaya <vijayaramachintha@gmail.com>
2026-03-05 09:47:12 +0530

    Add files via upload

---
+++ MavenProject/src/test/java/com/klu/MavenProject.java
@@ -0,0 +1,11 @@
+package com.klu;
+
+public class MavenProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi this is my new maven project");
+
+	}
+
+}
