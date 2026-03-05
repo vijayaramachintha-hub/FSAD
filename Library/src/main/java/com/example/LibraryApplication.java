0e2b4d1c1df1f4b2864a1bc485c84d47527e7aea
2400031510- vijaya <vijayaramachintha@gmail.com>
2026-03-05 09:48:58 +0530

    Add files via upload

---
+++ Library/src/main/java/com/example/LibraryApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
---
+++ Library/src/main/java/com/example/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
+
